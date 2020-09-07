d8e9d2db0fbf4cb9b4fbcc078352bf001a8576ca
kamilwowel11 <kamilr1998@o2.pl>
2020-09-07 16:54:19 +0200

    Initial commit

---
+++ Assets/Scripts/ProjectileMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+
+    public float speed;
+    public float fireRate;
+    public GameObject effectToSpawn;
+
+
+    
+
+    private bool bounced = false;
+
+    void Update()
+    {
+        if (speed != 0)
+        {
+            transform.position += transform.forward * (speed * Time.deltaTime*0.9f);
+        }
+        else
+        {
+            Debug.Log("No Speed");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag.Equals("BounceWall"))
+        {
+            speed -= 3;
+            Debug.Log("I hit BounceWall !");
+            Vector3 v = Vector3.Reflect(transform.forward, collision.contacts[0].normal);
+            transform.forward = v;
+            if (bounced == true)
+            {
+                Debug.Log("II Bounce ! Bye bye !");
+                speed = 0;
+                GameObject vfx;
+                
+                Destroy(gameObject);
+
+
+                vfx = Instantiate(effectToSpawn);
+                vfx.transform.position = transform.position;
+                Destroy(vfx, 2f);
+            }
+            bounced = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/SpawnProjectiles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectiles : MonoBehaviour
+{
+
+    public GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject>();
+
+    private GameObject effectToSpawn;
+    private float timeToFire = 0f;
+
+    void Start()
+    {
+        effectToSpawn = vfx[0];
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time >= timeToFire)
+        {
+            timeToFire = Time.time + 1 / effectToSpawn.GetComponent<ProjectileMove>().fireRate;
+            SpawnVFX();
+        }
+    }
+    void SpawnVFX()
+    {
+        GameObject vfx;
+        if (firePoint != null)
+        {
+            vfx = Instantiate(effectToSpawn);
+            vfx.transform.position = firePoint.transform.position + firePoint.transform.forward;
+            vfx.transform.forward = firePoint.transform.forward;
+        }
+        else
+        {
+            Debug.Log("No Fire Point");
+        }
+    }
+}
